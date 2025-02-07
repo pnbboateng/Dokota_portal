7b6d898f9c612f713d2bbe127224d13e6653a827
pnbboateng <nanaboatengphilip@gmail.com>
2025-02-07 21:42:27 +0000

    latest commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,20 @@ import React from "react"
 
 
  
-export default function AppSidebar(props) {
-  console.log(props.isShow)
+export default function Navbar(props) {
+  console.log(props)
 
   return (
     <div >
-    <div className={`fixed top-0 left-0 h-full w-[260px] bg-slate-50 shadow-2xl z-20 flex flex-col transform transition-transform duration-300 ${
-          props.isShow ? "translate-x-0" : "-translate-x-full"
-        }`}>
-     
-    </div>
+    {showSideBar && (
+            <div
+              className="fixed inset-0 bg-black bg-opacity-30 z-10"
+              onClick={() => setShowSideBar(false)}
+            ></div>
+          )}
+          <div className='h-[70px] border-b-2 border-b-stone-300 w-full  flex items-center'>
+                 <img className="block lg:hidden w-6 h-6 cursor-pointer" src={navtoggle} alt="nav toggle" onClick={() => setShowSideBar(true)} />
+              </div>
 
     </div>
   )
